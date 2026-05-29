6b817c5d399a0a3f49794f765dacd5f70bb1b3d3
Vaibhav Mishra <vaibhavmishrakpc@gmail.com>
2026-05-29 19:11:11 +0530

    style: make chat layout responsive using dvh units and hide footer conditionally on dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import DataLoaderIntro from './components/DataLoaderIntro';
 import Background3D from './components/Background3D';
 import HeaderNav from './components/HeaderNav';
@@ -17,33 +17,38 @@ import { CookiesPage, TermsPage, PrivacyPage } from './pages/CompliancePages';
 import { QueryProvider } from './context/QueryContext';
 import './index.css';
 
-const App: React.FC = () => (
-  <QueryProvider>
-    <div className="app-container">
-      <DataLoaderIntro />
-      <Background3D />
-      <div className="layout-border-left" />
-      <div className="layout-border-right" />
-      <HeaderNav />
-      <main className="main-content-layout" style={{ flexGrow: 1, position: 'relative', zIndex: 10 }}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/dashboard" element={<DashboardPage />} />
-          <Route path="/schema" element={<SchemaPage />} />
-          <Route path="/history" element={<HistoryPage />} />
-          <Route path="/docs" element={<DocsPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/admin" element={<AdminDashboard />} />
-          <Route path="/contact" element={<ContactPage />} />
-          <Route path="/cookies" element={<CookiesPage />} />
-          <Route path="/terms" element={<TermsPage />} />
-          <Route path="/privacy" element={<PrivacyPage />} />
-        </Routes>
-      </main>
-      <FooterNav />
-      <CookieBanner />
-    </div>
-  </QueryProvider>
-);
+const App: React.FC = () => {
+  const location = useLocation();
+  const hideFooter = location.pathname === '/dashboard' || location.pathname === '/admin';
+
+  return (
+    <QueryProvider>
+      <div className="app-container">
+        <DataLoaderIntro />
+        <Background3D />
+        <div className="layout-border-left" />
+        <div className="layout-border-right" />
+        <HeaderNav />
+        <main className="main-content-layout" style={{ flexGrow: 1, position: 'relative', zIndex: 10 }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/dashboard" element={<DashboardPage />} />
+            <Route path="/schema" element={<SchemaPage />} />
+            <Route path="/history" element={<HistoryPage />} />
+            <Route path="/docs" element={<DocsPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/admin" element={<AdminDashboard />} />
+            <Route path="/contact" element={<ContactPage />} />
+            <Route path="/cookies" element={<CookiesPage />} />
+            <Route path="/terms" element={<TermsPage />} />
+            <Route path="/privacy" element={<PrivacyPage />} />
+          </Routes>
+        </main>
+        {!hideFooter && <FooterNav />}
+        <CookieBanner />
+      </div>
+    </QueryProvider>
+  );
+};
 
 export default App;
